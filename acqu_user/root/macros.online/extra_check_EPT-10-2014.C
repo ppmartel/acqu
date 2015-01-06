2e06e6b9c740463a45863ee85d229dde51ebcc4d
Andreas Neiser <neiser@kph.uni-mainz.de>
2015-01-06 13:34:14 +0100

    User: Add one more plot to watch window

--- acqu_user/root/macros.online/extra_check_EPT-10-2014.C
+++ acqu_user/root/macros.online/extra_check_EPT-10-2014.C
@@ -1,7 +1,9 @@
 {
     TCanvas* watcher = new TCanvas("watcher","Watch Me");
 
-    watcher->Divide(2,1);
+    watcher->Divide(3,1);
+    watcher->SetWindowPosition(1920,500);
+    watcher->SetWindowSize(1700,700);
 
     watcher->cd(1);
     FPD_TimeOR->Draw();
@@ -9,6 +11,10 @@
     watcher->cd(2);
     gPad->SetLogz();
     NaI_Hits_v_TimeOR->Draw("colz");
+
+    watcher->cd(3);
+    gPad->SetLogz();
+    TwoD2000v1400->Draw("colz");
 
     TTimer* update_timer = new TTimer("watcher->Update();watcher->Draw();",2000);
 
