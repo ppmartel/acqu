de85bb6d43486a426a090def9da6fc224ed79ed4
Andreas Neiser <neiser@kph.uni-mainz.de>
2015-02-19 12:11:56 +0100

    User: Use new logbook from now on

--- acqu_user/root/macros.online/SaveSpectras.C
+++ acqu_user/root/macros.online/SaveSpectras.C
@@ -23,7 +23,7 @@ void SaveOpenSpectras() {
 	stringstream elog_cmd;
 	elog_cmd << "echo Run " << gAR->GetRunNumber() << " Online Spectra | ";
 	elog_cmd << "elog -h elog.office.a2.kph -u a2online a2messung ";
-	elog_cmd << "-l 'Main Group Logbook' -a Experiment='EPT test 2014-07' ";
+	elog_cmd << "-l 'Main Logbook 2015' -a Experiment='2015_03 DoublePol Deuteron' ";
 	elog_cmd << "-a Author='PLEASE FILL IN' -a Type=Routine ";
 	elog_cmd << "-a Subject='Online Spectra Run " << gAR->GetRunNumber() << "' ";
 	
