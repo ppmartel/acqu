b1fb159fafdc9cedaf15c794bebb5a1a83e75bb9
Phil Martel <philmartel0@gmail.com>
2015-05-07 17:16:50 +0200

    Core: Updated macros for G-E beamtime

---
+++ acqu_user/root/macros.online/extra_check_G-E-05-2015.C
@@ -0,0 +1,33 @@
+{
+    TCanvas* watcher = new TCanvas("watcher","Watch Me");
+
+    watcher->Divide(3,2);
+    watcher->SetWindowPosition(1920,500);
+    watcher->SetWindowSize(1500,1000);
+
+    watcher->cd(1);
+    gPad->SetLogy();
+    FPD_ScalerCurr->Draw();
+
+    watcher->cd(2);
+    FPD_TimeOR->Draw();
+
+    watcher->cd(3);
+    MWPC_Wires_Hits->Draw();
+
+    watcher->cd(4);
+    gPad->SetLogz();
+    NaI_ClPhi_v_ClTheta->Draw("colz");
+
+    watcher->cd(5);
+    gPad->SetLogz();
+    NaI_Hits_v_TimeOR->Draw("colz");
+
+    watcher->cd(6);
+    gPad->SetLogz();
+    TwoD2000v1400->Draw("colz");
+
+    TTimer* update_timer = new TTimer("watcher->Update();watcher->Draw();",4000);
+
+    update_timer->Start();
+}
