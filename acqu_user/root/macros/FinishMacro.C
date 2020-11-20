2550b2ad27240395b0b50b1f77e7b87d4221553d
Phil Martel <philmartel0@gmail.com>
2020-11-20 10:06:12 +0100

    Changes to FinishMacro for ROOT6 compatability

--- acqu_user/root/macros/FinishMacro.C
+++ acqu_user/root/macros/FinishMacro.C
@@ -1,14 +1,18 @@
-void FinishMacro(Char_t* file = "ARHistograms.root")
+void FinishMacro(TString file = "ARHistograms.root")
 {
   // Stuff to do at the end of an analysis run
   // Here all spectra are saved to disk
+  printf("Events: %d      Events Accepted: %d\n",
+	 gAN->GetNEvent(), gAN->GetNEvAnalysed() );
   printf("End-of-Run macro executing\n");
   TFile f(file,"recreate");
-  if( !f ){
-    printf("Open file %s for histogram save FAILED!!\n",file);
+  if( !f.IsOpen() ){
+    printf("Open file %s for histogram save FAILED!!\n",file.Data());
     return;
   }
   gROOT->GetList()->Write();
   f.Close();
-  printf("All histograms saved to %s\n\n",file);
+  printf("All histograms saved to %s\n\n",file.Data());
+
+  if (!(gAR->IsOnline())) gSystem->Exit(0);
 }
